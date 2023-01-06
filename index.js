a4638adc0ed3354a5546dbc046de0263857f713f
Anil <anilmangtani13@gmail.com>
2023-01-06 16:15:33 +0530

    demo changes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req, res)=>{
+    res.send("Server is running on "+PORT);
+})
+
+app.get('/number',(req, res)=>{
+    res.send("" + Math.random(0));
+})
+
+app.post('/create',(req,res)=>{
+    console.log(req.body);
+    res.send("User Created again");
+})
+
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on "+ PORT);
+});
